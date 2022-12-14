dec116336552e3fe16e070eb9be6d68d942950c4
K.J <117104691+K-Jor162@users.noreply.github.com>
2022-12-14 13:03:45 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+var i;
+var nameBrands = ["Nike", "Addidas", "vans", "Timberlands"];
+for (i = 0; i < 30; i++) {
+  console.log(nameBrands[i]);
+}
+var brandInfo = [
+  {
+    name: "Nike",
+    product: "Jordan retro-7s",
+    price: "$150"
+  },
+  {
+    name: "Addidas",
+    product: "Addidas track suite",
+    price: "$80"
+  },
+  {
+    name: "Vans",
+    product: "shoes",
+    price: "$60"
+  },
+  {
+    name: "Timberland",
+    product: "boots",
+    price: "$80"
+  }
+];
